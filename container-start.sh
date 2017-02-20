d767d50359bd23d0c3607a974f421a244ee0dafe
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-02-20 17:22:43 +0000

    Don't exit after starting the notebook service

--- container-start.sh
+++ container-start.sh
@@ -14,6 +14,4 @@ cp -nR /home/jovyan/ /home/$USER/
 chown -R $USER /home/$USER
 
 # Disable authentication, see: https://github.com/jupyter/docker-stacks/blob/master/datascience-notebook/README.md#notebook-options
-start-notebook.sh --NotebookApp.token='' &
-
-touch /tmp/ready
+start-notebook.sh --NotebookApp.token=''
