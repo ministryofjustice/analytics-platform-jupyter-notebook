5abcb08560ace4001ae40df5e5b7c26d08329dc2
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-02-20 14:52:14 +0000

    Touching /tmp/ready readiness probe for k8s
    See: https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-probes/#defining-readiness-probes

--- container-start.sh
+++ container-start.sh
@@ -14,4 +14,6 @@ cp -nR /home/jovyan/ /home/$USER/
 chown -R $USER /home/$USER
 
 # Disable authentication, see: https://github.com/jupyter/docker-stacks/blob/master/datascience-notebook/README.md#notebook-options
-start-notebook.sh --NotebookApp.token=''
+start-notebook.sh --NotebookApp.token='' &
+
+touch /tmp/ready
