0b3a4c511fdff6ab194d0fd10188ee002e76e2f2
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-02-20 14:17:31 +0000

    Trying to change ownership of CONDA_DIR in docker image not at start
    As this step is REALLY slow it's better if we run it once when we build the
    container. Giving ownership to the whole 'staff' group should be enough.

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,11 @@ RUN jupyter dashboards quick-setup --sys-prefix
 RUN jupyter nbextension enable jupyter_dashboards --py --sys-prefix
 
 USER root
+
+# Changing ownership of $CONDA_DIR to all users in 'staff'
+# (so we don't have to do it at container start)
+RUN chown -R $NB_USER:staff $CONDA_DIR
+
 COPY container-start.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/container-start.sh
 
--- container-start.sh
+++ container-start.sh
@@ -12,8 +12,6 @@ useradd -g $GROUP -u $USER_UID -d /home/$USER $USER
 # Copy jovyan's files in the home without overwriting (-n)
 cp -nR /home/jovyan/ /home/$USER/
 chown -R $USER /home/$USER
-#### TEMPORARY - Testing if this makes the app start
-# chown -R $USER /opt/conda
 
 # Disable authentication, see: https://github.com/jupyter/docker-stacks/blob/master/datascience-notebook/README.md#notebook-options
 start-notebook.sh --NotebookApp.token=''
