773d7972d098d5a235d51e58b5187ffbd39e3b47
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-10-13 12:10:11 +0100

    spark config

--- files/pyspark-s3.py
+++ files/pyspark-s3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.amazonaws:aws-java-sdk:1.10.34,org.apache.hadoop:hadoop-aws:2.6.0 pyspark-shell'
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.amazonaws:aws-java-sdk:1.7.4,org.apache.hadoop:hadoop-aws:2.7.1 pyspark-shell'
 
 import pyspark
 sc = pyspark.SparkContext("local[*]")
@@ -10,4 +10,4 @@ from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 
 hadoopConf = sc._jsc.hadoopConfiguration()
-hadoopConf.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+hadoopConf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
