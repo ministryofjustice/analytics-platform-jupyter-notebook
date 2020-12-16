65cf5c7dc31e53b1ba2cdd3709d32066ad5dd9f5
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2020-12-16 10:51:11 +0000

    allspark: removed deprecated SQLContext and updated example
    The `pyspark-s3.py` script was importing `pyspark.sql.SQLContext` (now
    deprecated) and declaring an unused variable. Removed to avoid confusion.
    
    Also updated the `spark_read_s3.py` test code snippet with something
    more complete that almost works when copied and pasted (e.g. in a
    running JupyterLab instance)

--- allspark-notebook/files/pyspark-s3.py
+++ allspark-notebook/files/pyspark-s3.py
@@ -3,8 +3,5 @@
 import pyspark
 sc = pyspark.SparkContext("local[*]")
 
-from pyspark.sql import SQLContext
-sqlContext = SQLContext(sc)
-
 hadoopConf = sc._jsc.hadoopConfiguration()
 hadoopConf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
